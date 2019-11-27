076e558eab47ed00b68a1be4895d7b26963cf21d
umpatil <54542713+umpatil@users.noreply.github.com>
2019-11-27 17:25:34 +0530

    Added UemshPatilNew.java

---
+++ bin/com/java/arraylist/UmeshPatil.java
@@ -0,0 +1,11 @@
+package com.java.arraylist;
+
+public class UmeshPatil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Umesh");
+	}
+
+}
